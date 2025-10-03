3f38a35d3b0a976c22f0a8ead52006eb9a37d77e
check4game <23149814+check4game@users.noreply.github.com>
2025-10-03 16:26:00 +0300

    Create MTBlake3.c

---
+++ MTBlake3.c
@@ -0,0 +1,127 @@
+#include ".\BLAKE3\c\blake3.h"
+#include <errno.h>
+#include <stdio.h>
+#include <string.h>
+#include <io.h>
+#include <fcntl.h>
+#include <time.h>
+
+extern int mt_selector;
+
+extern int tbb_max_concurrency();
+
+static unsigned char buf[128 * 1024];
+
+static int TEST(char * file, int mt)
+{
+#if !defined(BLAKE3_USE_TBB)
+	mt = 0;
+#endif
+
+	if (mt < 0 || mt > 3) mt = 0;
+
+	mt_selector = mt;
+
+	int fn = _open(file, _O_RDONLY | _O_BINARY);
+
+	if (fn < 0)
+	{
+		fprintf(stderr, "open failed: %s\n", strerror(errno)); return 0;
+	}
+
+	// Initialize the hasher.
+	blake3_hasher hasher;
+	blake3_hasher_init(&hasher);
+
+	clock_t start = clock();
+
+	size_t file_size = 0;
+	while (1)
+	{
+		size_t n = read(fn, buf, sizeof(buf));
+
+		file_size += n;
+
+		if (n > 0)
+		{
+#if !defined(BLAKE3_USE_TBB)
+			blake3_hasher_update(&hasher, buf, n);
+#else
+			if (mt)
+				blake3_hasher_update_tbb(&hasher, buf, n);
+			else
+				blake3_hasher_update(&hasher, buf, n);				
+#endif
+		}
+		else if (n == 0)
+		{
+			break; // end of file
+		}
+		else
+		{
+			fprintf(stderr, "read failed: %s\n", strerror(errno));
+			return 1;
+		}
+	}
+
+	_close(fn);
+
+	// Finalize the hash. BLAKE3_OUT_LEN is the default output length, 32 bytes.
+	uint8_t output[BLAKE3_OUT_LEN];
+	blake3_hasher_finalize(&hasher, output, BLAKE3_OUT_LEN);
+	clock_t end = clock();
+
+	double elapsed = ((double)(end - start)) / CLOCKS_PER_SEC;
+
+	if (!mt)
+	{
+		printf("SGL");
+	}
+	else if (mt == 1)
+	{
+		printf("TBB");
+	}
+	else if (mt == 2)
+	{
+		printf("TBB");
+	}
+	else if (mt == 3)
+	{
+		printf("OMP");
+	}
+
+	printf(", %2d Threads, ", tbb_max_concurrency());
+
+	printf("Time: %.6f sec, %llu, Hash: ", elapsed, file_size);
+
+	// Print the hash as hexadecimal.
+	for (size_t i = 0; i < BLAKE3_OUT_LEN; i++) {
+		printf("%02x", output[i]);
+	}
+	printf("\n"); return 0;
+}
+
+int main(int argc, char** argv)
+{
+	if (argc != 2)
+	{
+		printf("%s file.ext\n", argv[0]); return 0;
+	}
+
+	int fn = _open(argv[1], _O_RDONLY | _O_BINARY);
+
+	if (fn < 0)
+	{
+		fprintf(stderr, "open failed: %s\n", strerror(errno)); return 0;
+	}
+
+	_close(fn);
+
+	for (int i = 0; i < 4; i++)
+	{
+		TEST(argv[1], i);
+		TEST(argv[1], i);
+		TEST(argv[1], i);
+		printf("\n");
+	}
+}
